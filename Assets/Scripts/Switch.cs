0353ae38ec7207ff1b863759d4388d4fc912376e
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2024-10-18 02:13:42 -0400

    Implemented switch animation

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -8,6 +8,9 @@ public class Switch : MonoBehaviour
     [field: SerializeField]
     public Gate Gate { get; private set; }
 
+    [SerializeField]
+    private Animator _animator;
+
     private void Start()
     {
         foreach (EventCollider collider in GetComponentsInChildren<EventCollider>())
@@ -18,8 +21,8 @@ public class Switch : MonoBehaviour
 
     private void ColliderNode_Collision(object sender, CollisionEventArgs e)
     {
-        Debug.Log($"Switch Activated: {gameObject.name}");
-        Gate.ToggleOpenClose();
-        
+        // Debug.Log($"Switch Activated: {gameObject.name} ({(Gate.IsOpen ? "Off -> On" : "On -> Off")})");
+        _animator.Play(Gate.IsOpen ? "Off" : "On", layer: 0);
+		Gate.ToggleOpenClose();
     }
 }
